2b6f11ad88668dc79c7044bf4becaeb7d3dcfc2c
Thilakpriyan <rameshthilakpriyan@gmail.com>
2025-12-30 12:39:03 +0530

    ANALYSING THE CHAT!

--- app.py
+++ app.py
@@ -1,2 +1,39 @@
 import streamlit as st
+
+import helper
+import prepocessor
 st.sidebar.title('WHATSAPP CHAT ANALYZER')
+
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    data= bytes_data.decode("utf-8")
+    df=prepocessor.prepocesses(data)
+
+    st.dataframe(df)
+    #fetch unique users
+    user_list=df['user'].unique().tolist()
+    user_list.remove('group_notification')
+    user_list.sort()
+    user_list.insert(0,'Overall')
+
+    selected_user=st.sidebar.selectbox('SHOW ANALYZIS ',user_list)
+    if st.sidebar.button('Show Analysis'):
+        num_messages,words,media_message,links= helper.fetch_stats(selected_user,df)
+
+        col1,col2,col3,col4 =st.columns(4)
+
+        with col1:
+            st.header('Totalmessages')
+            st.title(num_messages)
+        with col2:
+            st.header('Total Words')
+            st.title(words)
+        with col3:
+            st.header('Media Shared')
+            st.title(media_message)
+        with col4:
+            st.header('Links Shared')
+            st.title(links)
--- prepocessor.py
+++ prepocessor.py
@@ -1,3 +1,6 @@
+import re
+import pandas as pd
+
 def prepocesses(data):
     pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s?(?:AM|PM)'
     messages = re.split(pattern, data)
